1cd7a0287afff9556e222a70733f3a8bfb555b14
zuiyu1998 <1542844298@qq.com>
2025-06-03 21:41:47 +0800

    feat: init

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use bevy::prelude::*;
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, _app: &mut App) {}
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use bevy::prelude::*;
+use turn_base_demo::GamePlugin;
+
+fn main() {
+    let mut app = App::new();
+
+    app.add_plugins((DefaultPlugins, GamePlugin));
+
+    app.run();
+}
