2ecc14976b0ffb0249865089cd4800450e646ebf
zuiyu1998 <1542844298@qq.com>
2025-06-03 22:26:46 +0800

    feat: 添加 AttributeModifier

--- src/attribute_system/attribute.rs
+++ src/attribute_system/attribute.rs
@@ -1,8 +1,40 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum ModifierOperation {
+    Absolute,
+    Override,
+    Percentage,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct AttributeModifier {
+    pub source_uid: String,
+    value: f32,
+    operation: ModifierOperation,
+}
+
+impl AttributeModifier {
+    pub fn new(value: f32, operation: ModifierOperation, source_uid: String) -> Self {
+        Self {
+            value,
+            operation,
+            source_uid,
+        }
+    }
+
+    pub fn apply(&self, attr: &mut Attribute) {
+        match self.operation {
+            ModifierOperation::Absolute => attr.current_value += self.value,
+            ModifierOperation::Override => attr.current_value = self.value,
+            ModifierOperation::Percentage => attr.current_value = attr.base_value * self.value,
+        }
+    }
+}
+
 /// 属性
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct AttributeBase {
+pub struct Attribute {
     /// 属性的唯一标识名
     pub attribute_name: String,
     /// 属性的显示名称
@@ -19,9 +51,10 @@ pub struct AttributeBase {
     base_value: f32,
     /// 属性的当前值
     current_value: f32,
+    modifier_list: Vec<AttributeModifier>,
 }
 
-impl AttributeBase {
+impl Attribute {
     pub fn get_base_value(&self) -> f32 {
         self.base_value
     }
